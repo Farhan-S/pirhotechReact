7fee32f0f9b60e7b2225bb4ffb90426cb4880123
mdnuruzzamanemon <mdnuruzzamanemon@gmail.com>
2023-11-22 23:41:04 +0600

    new

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,6 +4,7 @@ import ParticleBG from "./particleBG";
 import OurServices from "./ourServices";
 import StatusCounter from "./statusCounter";
 import Products from "./products";
+import headerImage from "../images/headerIgame.png";
 
 
 
@@ -16,27 +17,38 @@ function Index() {
 
     return (
         <div className="main">
+            <ParticleBG />
             <div className="headerDes">
-                <ParticleBG />
+
                 <div className="headerDesMid">
-                    <h1 className="comName">PiRhoTech</h1>
-                    <p>A Bit of Spark</p>
-                    <h1>We provide you {" "}
-                        <span>{text}</span>
-                        <Cursor 
-                        cursorColor='red' 
-                        cursorStyle='>_'
-                        />
-                        Services
-                    </h1>
+                    <div className="curly-brace"></div>
+                    <div className="headerText">
+                        <h1 className="comName">PiRhoTech</h1>
+                        <p>We believe in imagination</p>
+                        <h1>We provide you {" "}
+                        </h1>
+                        <h1>
+                            <span>{text}</span>
+                            <Cursor
+                                cursorColor='red'
+                                cursorStyle='>_'
+                            />
+                            
+                        </h1>
+                        <h1>Services</h1>
+                    </div>
+                    <div className="headerImage">
+                        <img src={headerImage} alt="headerImage" />
+                    </div>
+
                 </div>
             </div>
             <OurServices />
             <Products />
             <StatusCounter />
-            
 
-            
+
+
         </div>
 
 
